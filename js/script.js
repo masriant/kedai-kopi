35fe2abc6971f7214a7f45833e5791f9b3cafbc5
masriant <info.masrianto@gmail.com>
2023-01-20 08:49:07 +0700

    membuat website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik di luar sidebar untuk menghilangkkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
